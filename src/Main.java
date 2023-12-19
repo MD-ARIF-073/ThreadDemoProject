36d29793285c0ecfa1d6fccea35afba8d3348bc5
Mohammad Arif Ishtiaq <arifsani73@gmail.com>
2023-12-19 19:17:53 +0600

    Implementing runnable interface and converting anonymous class into lambda expression to handle multiple threads

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-
+/* Creating threads by extending Thread class and by implementing Runnable interface */
 class Nonte extends Thread{
     public void run(){
         for (int i = 1; i<=50; i++)
@@ -13,9 +13,23 @@ class Nonte extends Thread{
     }
 }
 
-class Fonte extends Thread{
+class Nonte implements Runnable{
     public void run(){
-        for (int i = 1; i<=50; i++)
+        for (int i = 1; i<=5; i++)
+        {
+            System.out.println("Thread");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
+class Fonte implements Runnable{
+    public void run(){
+        for (int i = 1; i<=5; i++)
         {
             System.out.println("Completable Future");
             try {
@@ -30,23 +44,58 @@ class Fonte extends Thread{
 public class Main {
     public static void main(String[] args) {
 
-        Nonte n = new Nonte();
-        Fonte f = new Fonte();
+        Runnable n = new Nonte();
+        Runnable n = new Runnable() {
+            public void run(){
+                for (int i = 1; i<=5; i++)
+                {
+                    System.out.println("Thread");
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        };
+
+/* Converting anonymous class into lambda expression */
+        Runnable n = () ->                            // Using lambda expression
+        {
+                for (int i = 1; i<=5; i++)
+                {
+                    System.out.println("Thread");
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+        };
+
+
+        Runnable f = new Fonte();
+
+        Thread t1 = new Thread(n);
+        Thread t2 = new Thread(f);
 
         /* Priority set for various threads */
-        //System.out.println(n.getPriority());
-       // f.setPriority(10);
+        System.out.println(n.getPriority());
+        f.setPriority(10);
         f.setPriority(Thread.NORM_PRIORITY);
         n.setPriority(Thread.MIN_PRIORITY);
         n.setPriority(Thread.MAX_PRIORITY);
 
-        n.start();
+        n.start();   // start method coming from thread class
         try {
             Thread.sleep(2);     // two start method will be executed with a gap of 2 mili second
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
         f.start();
+
+        t1.start();
+        t2.start();
 
     }
 
